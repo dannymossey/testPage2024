9c7733dec779806af0adc063faa8391a36cdd4c7
Daniel Mossey <63306200+dannymossey@users.noreply.github.com>
2024-02-10 18:26:21 +0000

    Update header section of index.html (generated by blackbox.ai )- next stop add js

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.getElementById('hamburger').addEventListener('click', function() {
+//     document.getElementById('navigation-items').classList.toggle('active');
+//     // document.getElementById('openHam').style.display = 'none';
+//     // document.getElementById('closeHam').style.display = 'inline-block';
+
+
+
+//   });
