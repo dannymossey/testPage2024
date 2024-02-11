1a13965d7007b4ed16ca860e1d37254ff87760c3
Daniel Mossey <63306200+dannymossey@users.noreply.github.com>
2024-02-11 11:12:25 +0000

    image file path change

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 const hamburger = document.getElementById('hamburger').addEventListener('click', function() {
   document.getElementById('navigation-items').classList.toggle('active');
   console.log('hello')
+  
 
 })
