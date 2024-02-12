c4a8209568b5e9d9f808963b2bc11b3e394c5f28
Daniel Mossey <63306200+dannymossey@users.noreply.github.com>
2024-02-12 21:53:15 +0000

    tranistion to body when page loads

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
+let bar = document.querySelectorAll('.bar');
 const hamburger = document.getElementById('hamburger').addEventListener('click', function() {
   document.getElementById('navigation-items').classList.toggle('active');
-  console.log('hello')
   
+});
 
-})
+
