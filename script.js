bd2e2d18af589a03c1bfd0835e19a55a6b10e878
Daniel Mossey <63306200+dannymossey@users.noreply.github.com>
2024-02-13 19:20:19 +0000

    hamburger open navigation - help from ai

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 const barOne = document.getElementById('bar-one');
 const hamburger = document.getElementById('hamburger').addEventListener('click', function() {
   let navItems = document.getElementById('navigation-items').classList.toggle('active');
-  // let navClass = document.querySelector('.navigation-items');
   barOne.classList.toggle('active');
-  // navClass.classList.add('fade-in');
+  
  
 
 
