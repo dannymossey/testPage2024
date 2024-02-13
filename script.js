0ff2b67c9940f242d286392e7e502024730d58d5
Daniel Mossey <63306200+dannymossey@users.noreply.github.com>
2024-02-13 17:30:44 +0000

    hamburger clean up part 1

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let bar = document.querySelectorAll('.bar');
+
+const barOne = document.getElementById('bar-one');
 const hamburger = document.getElementById('hamburger').addEventListener('click', function() {
-  document.getElementById('navigation-items').classList.toggle('active');
-  
+  let navItems = document.getElementById('navigation-items').classList.toggle('active');
+  // let navClass = document.querySelector('.navigation-items');
+  barOne.classList.toggle('active');
+  // navClass.classList.add('fade-in');
+ 
+
+
+
 });
 
 
